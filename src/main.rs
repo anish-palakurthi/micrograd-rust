c3a81829c615354788ae87d313e32ceb8a995545
Anish Palakurthi <anish.palakurthi@gmail.com>
2024-08-04 22:35:37 -0700

    added backwards closure

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,63 @@
 use std::collections::HashSet;
 use std::fmt::*;
 use std::fmt;
+use std::ops;
+
 struct Value {
     data: f32, 
     grad: f32, 
-    backward: f32,
-    prev: HashSet<usize>,
-    operator: String 
+    prev: Vec<usize>,
+    operator: String,
+    backward: Box<dyn FnMut()>,
 }
 
 impl Value {
-    fn new(data: f32, grad: f32, backward: f32, prev: Vec<usize>, operator: String) -> Self{
+    fn new(data: f32, prev: Vec<usize>, operator: String) -> Self {
         Self {
             data,
-            grad,
-            backward,
-            prev: prev.into_iter().collect(),
-            operator
-
+            grad: 0.0,
+            prev,
+            operator,
+            backward: Box::new(|| {}),
         }
     }
-
 }
+
+impl ops::Add<Value> for Value {
+    type Output = Value;
+
+    fn add(mut self, mut rhs: Value) -> Value {
+        let sum_data = self.data + rhs.data;
+
+        let mut res = Value::new(sum_data, vec![&self as *const _ as usize, &rhs as *const _ as usize],  "+".to_string());
+
+        let backward = {
+            let res_grad = res.grad;
+            move || {
+                self.grad += res_grad;
+                rhs.grad += res_grad;
+            }
+        };
+
+        res.backward = Box::new(backward);
+        res
+    }
+}
+
 impl Display for Value{
 
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
 
-        write!(f, "Data:{}; grad:{};  backward:{};  prev:{:#?};  operator: {}", self.data, self.grad, self.backward, self.prev, self.operator)
-
+        write!(f, "Data:{}; grad:{};  prev:{:#?};  operator: {}", self.data, self.grad, self.prev, self.operator)
     }
-
 }
 
 
 
 fn main(){
 
-    let example = Value::new(1.0, 2.0, 3.0, vec![1,2], "+".to_string());
+    let example = Value::new(1.0, vec![1, 2], "+".to_string());
 
     println!("Example!: {}", example);
-
-
 
 }
